0aaf8bad5023434cc7a9b7679a24844110efe077
JAE HOON LEE <jazzyrain722@naver.com>
2025-04-18 22:01:59 +0900

    amplify설정

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,14 @@
 const nextConfig = {
   images: {
     domains: ['imgur.com', 'i.imgur.com'],
+    unoptimized: true,
+  },
+  output: 'standalone',
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
   },
 }
 
